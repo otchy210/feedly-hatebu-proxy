c3213bab094757611ff483e9ab8659f040e2b4b5
Otchy <id+github@otchy.net>
2022-12-31 17:55:18 +0900

    1.0.0: Add Mac package

--- main.js
+++ main.js
@@ -2,11 +2,27 @@ const { app, Tray, Menu } = require('electron');
 const path = require('path');
 const { startProxy } = require('./proxy');
 
+const getPlatform = () => {
+  switch(process.platform) {
+    case 'win32':
+      return 'Windows';
+    case 'darwin':
+      return 'Mac';
+    default:
+      return 'Unsupported';
+  }
+}
+
 // need to put it in the global context to avoid GC
 let tray;
 const addTray = () => {
   // support Windows and Mac only
-  const trayImage = process.platform === 'win32' ? 'tray-win.ico' : 'tray-mac.png';
+  const platform = getPlatform();
+  if (platform === 'Unsupported') {
+    throw new Error('Unsupported platform');
+  }
+  
+  const trayImage = platform === 'Windows' ? 'tray-win.ico' : 'tray-mac.png';
   const trayImagePath = path.join(__dirname, 'images', trayImage);
   tray = new Tray(trayImagePath);
   tray.setToolTip('Feedly はてブ Proxy');
@@ -15,9 +31,18 @@ const addTray = () => {
   ]));
 }
 
+const hideDockIcon = () => {
+  const platform = getPlatform();
+  if (platform !== 'Mac') {
+    return;
+  }
+  app.dock.hide();
+}
+
 const main = async () => {
   await app.whenReady();
   addTray();
+  hideDockIcon();
   startProxy();
 }
 main();
