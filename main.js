7a8c73ee96e644b2ef1ca2d2f72202dcc9cdb3f9
Otchy <id+github@otchy.net>
2022-12-31 15:13:32 +0900

    1.0.0: Add tray icon

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-const { app, Tray, Menu, nativeImage } = require('electron');
+const { app, Tray, Menu } = require('electron');
 const path = require('path');
 const { startProxy } = require('./proxy');
 
 // need to put it in the global context to avoid GC
 let tray;
 const addTray = () => {
-  const icon = nativeImage.createFromPath(path.join(__dirname, 'images/tray.png'));
-  tray = new Tray(icon);
+  // support Windows and Mac only
+  const trayImage = process.platform === 'win32' ? 'tray-win.ico' : 'tray-mac.png';
+  const trayImagePath = path.join(__dirname, 'images', trayImage);
+  tray = new Tray(trayImagePath);
+  tray.setToolTip('Feedly はてブ Proxy');
   tray.setContextMenu(Menu.buildFromTemplate([
-    { label: 'Quit', type: 'normal', click: () => app.quit() },
+    { label: '終了', type: 'normal', click: () => app.quit() },
   ]));
 }
 
